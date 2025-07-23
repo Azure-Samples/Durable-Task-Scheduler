5b1571623e9336aa01622fc3a76eaa7b8c23fa50
greenie-msft <nigreenf@microsoft.com>
2025-07-23 10:05:32 -0700

    Address more PR feedback

--- samples/durable-task-sdks/dotnet/Agents/PromptChaining/Worker/Services/AgentServices.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.Logging;
-
-namespace AgentChainingSample.Services;
-
-// Note: Implementation classes have been moved to separate files:
-// - ResearchAgentService.cs
-// - ContentGenerationAgentService.cs 
-// - ImageGenerationAgentService.cs
-// - Models/DalleModels.cs
